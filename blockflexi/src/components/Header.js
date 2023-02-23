5c60b7e33422fdb8928193f2c178cb832337183b
Ashwath-learner1 <ashwathkumaruumapathy@gmail.com>
2023-02-23 13:20:38 +0530

    changed color

--- blockflexi/src/components/Header.js
+++ blockflexi/src/components/Header.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button,Popover,List,ListItem,ListItemText } from '@mui/material';
 
 function Header() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+      };
+    
+      const handleClose = () => {
+        setAnchorEl(null);
+      };
+
+      const open = Boolean(anchorEl);
+      const id = open ? 'dropdown-menu' : undefined;
+    
   return (
-    <AppBar position="static">
+    <div className="header">
+    <AppBar position="static" style={{ background: '#9A1B56' }}>
       <Toolbar>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
-          Blockflexi
+        <Typography variant="h4" style={{ marginRight:"auto" }}>
+          BLOCKFLEXI
         </Typography>
         <Button color="inherit">Home</Button>
         <Button color="inherit">Contact</Button>
         <Button color="inherit">Schemes</Button>
+        
         <Button color="inherit">Profile</Button>
       </Toolbar>
     </AppBar>
+    
+    </div>
   );
 }
 
