45cd5f24d619541e9950f4782d0b63cd870be090
Ashwath-learner1 <ashwathkumaruumapathy@gmail.com>
2023-02-27 10:33:27 +0530

    new commit

--- blockflexi/src/components/Header.js
+++ blockflexi/src/components/Header.js
@@ -9,7 +9,8 @@ import {
   ListItem,
   ListItemText,
   Menu,
-  MenuItem
+  MenuItem,
+  Stack
 } from "@mui/material";
 import {Card,CardContent,CardActions} from '@mui/material';
 
@@ -43,10 +44,11 @@ function Header() {
   };
 
   
+  
   return (
     <div className="header">
-      <AppBar position="static" style={{ background: "#9A1B56" }}>
-        <Toolbar>
+      <AppBar position="static" style={{ background: "#9A1B56",display:'flex',flexDirection:'column' }}>
+        <Toolbar style={{display:'flex',flexFlow: 'row wrap',justifyContent:'center',alignItems:'center'}}>
           <Typography variant="h4" style={{ marginRight: "auto" }}>
             BLOCKFLEXI
           </Typography>
@@ -106,69 +108,38 @@ function Header() {
       </Menu>
         </Toolbar>
       </AppBar>
-    {/* <AppBar position="static" style={{ background: '#9A1B56' }}>
-      <Toolbar>
-        <Typography variant="h4" style={{ marginRight:"auto" }}>
-          BLOCKFLEXI
-        </Typography>
-        <Button color="inherit">Home</Button>
-        <Button color="inherit">Contact</Button>
-        <Button color="inherit" >Schemes </Button>
-        <Button color="inherit" onClick={handleClick}>Gold Price</Button>
-        <Popover
-          id={id}
-          open={open}
-          anchorEl={anchorEl}
-          onClose={handleClose}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'center',
-          }}
-        >
-          <List>
-            <ListItem >
-              <ListItemText primary="Current Gold Price per gm" style={listitemtext}/>
-            </ListItem>
-            <ListItem >
-              <ListItemText primary="Rs.22,500" style={{fontSize:'14px',
-        fontWeight:'bold',
-        color:'#000'}}/>
-            </ListItem>
-            
-          </List>
-        </Popover>
-        <Button color="inherit">Profile</Button>
-      </Toolbar>
-    </AppBar> */}
-    <Card style={{ margin: '150px  550px 0  70px',Width:'400px'}} variant="outlined" raised="true">
+   
+    <div style={{ display: 'flex' ,flexWrap:'row wrap'}}>
+    <Card style={{ Width:'400px',display: 'flex' ,flexDirection:'column',margin:'120px 0 0 40px'}} variant="outlined" >
         <CardContent>
           <Typography variant="h4" component="h2" style={{ marginRight:"320px",marginBottom:"12px"  }}>
             Welcome to BlockFlexi
           </Typography>
-          <Typography variant="h6" component="h2" style={{ marginRight:"130px" }}>
+          <Typography variant="h6" component="h2" style={{ textAlign:'justify'  }}>
           Introducing Block-Flexi: The Future of Jewellery Ownership. 
           </Typography>
-          <Typography variant="h6" component="h2" style={{ marginRight:"20px" }}>
+          <Typography variant="h6" component="h2" style={{ textAlign:'justify'  }}>
             A Revolutionary Blockchain Solution for a Flexible Jewellery Acquisition.
           </Typography>
+  
         </CardContent>
-        <CardActions style={{ display: 'flex' }}>
-          <Button size="large" variant="outlined"  style={{marginLeft:"30px",width:"200px"}}>
+        <CardActions>
+          <Stack spacing={2} direction='row'>
+      <Button size="large" variant="outlined"  style={{width:"200px"  }}>
             Customer
           </Button>
-          <Button size="large"  variant="outlined"   style={{marginLeft:"20px",width:"200px"}}>
+          <Button size="large"  variant="outlined"   style={{width:"200px"}}>
             Jeweller
           </Button>
-          <Button size="large"  variant="outlined"   style={{marginLeft:"20px",width:"200px"}}>
+          <Button size="large"  variant="outlined"   style={{width:"200px"}}>
             Bank
           </Button>
+          </Stack>
         </CardActions>
+       
+        
       </Card>
-    
+      </div>
     </div>
   );
 }
